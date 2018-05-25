2ac6dda8d799c19c8be33ed03067de5329211825
Poaz <Dines@rxdesign.dk>
2018-05-25 07:02:25 +0200

    SVM update

--- ConationNetwork/ScikitSVM.py
+++ ConationNetwork/ScikitSVM.py
@@ -4,33 +4,51 @@ import pandas as pd
 from sklearn import svm
 import pickle as pickle
 
-def load_data(label_name='ConationLevel'):
-
+def load_Train_Test_Data():
     CSV_COLUMN_NAMES = ['Gaze 3D position left X', 'Gaze 3D position left Y', 'Gaze 3D position left Z',
                         'Gaze 3D position right X', 'Gaze 3D position right Y', 'Gaze 3D position right Z',
-                        'Pupil diameter left', 'Pupil diameter right', 'HR', 'GSR', 'ConationLevel', 'PredictedConation']
+                        'Pupil diameter left', 'Pupil diameter right', 'HR', 'GSR', 'ConationLevel']
+
+    CSV_COLUMN_NAMES_TEST = ['Gaze 3D position left X', 'Gaze 3D position left Y', 'Gaze 3D position left Z',
+                        'Gaze 3D position right X', 'Gaze 3D position right Y', 'Gaze 3D position right Z',
+                        'Pupil diameter left', 'Pupil diameter right', 'HR', 'GSR', 'ConationLevel',
+                        'PredictedConation', 'GameState', 'TimeSinceStart']
 
 
-    train_path = "CombinedData_Data2.csv"
+    train_path = "TrainData.csv"
 
     # Parse the local CSV file.
     train = pd.read_csv(filepath_or_buffer=train_path,
                         names=CSV_COLUMN_NAMES,
                         header=0, sep=',')
 
-    train_features, test_features = sk.train_test_split(train, test_size=0.20, random_state=42)
-    train_features = train_features.drop(['ConationLevel'], axis=1)
-    test_features = test_features.drop(['ConationLevel'], axis=1)
-    train_features = train_features.drop(['PredictedConation'], axis=1)
-    test_features = test_features.drop(['PredictedConation'], axis=1)
+    train_feature = train.drop(['ConationLevel'], axis=1)
 
-    train_label, test_label = sk.train_test_split(train.pop(label_name), test_size=0.20, random_state=42)
+    train_label = train.pop('ConationLevel')
+    train_label = train_label.replace([1, 2, 3, 4], 0)
+    train_label = train_label.replace([5, 6, 7], 1)
 
-    # Return four DataFrames.
-    return (train_features, train_label), (test_features, test_label)
+    test_path = "TestData.csv"
+
+    # Parse the local CSV file.
+    test = pd.read_csv(filepath_or_buffer=test_path,
+                        names=CSV_COLUMN_NAMES_TEST,
+                        header=0, sep=',')
+
+    test_feature = test.drop(['ConationLevel'], axis=1)
+    test_feature = test_feature.drop(['PredictedConation'], axis=1)
+    test_feature = test_feature.drop(['GameState'], axis=1)
+    test_feature = test_feature.drop(['TimeSinceStart'], axis=1)
+
+    test_label = test.pop('ConationLevel')
+    test_label = test_label.replace([1, 2, 3, 4], 0)
+    test_label = test_label.replace([5, 6, 7], 1)
 
 
-(train_feature, train_label), (test_feature, test_label) = load_data()
+    return(train_feature, train_label), (test_feature, test_label)
+
+
+(train_feature, train_label), (test_feature, test_label) = load_Train_Test_Data()
 
 
 clf = svm.SVC()
